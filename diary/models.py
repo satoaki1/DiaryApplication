a1a59830f94a78cac6c2bd7510cb4a02877e07fb
ihsa332019 <satoakiishihara@sd.taylors.edu.my>
2022-12-20 16:47:13 +0900

    edited index.html.

--- diary/models.py
+++ diary/models.py
@@ -5,8 +5,8 @@ import uuid
 
 class Diary(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    date = models.DateField(verbose_name='日付', default=timezone.now)
-    title = models.CharField(verbose_name='タイトル', max_length=40)
-    text = models.CharField(verbose_name='本文', max_length=200)
-    created_at = models.DateTimeField(verbose_name='作成日時', default=timezone.now)
-    updated_at = models.DateTimeField(verbose_name='編集日時', blank=True, null=True)
+    date = models.DateField(verbose_name='Date', default=timezone.now)
+    title = models.CharField(verbose_name='Title', max_length=40)
+    text = models.CharField(verbose_name='Bio', max_length=200)
+    created_at = models.DateTimeField(verbose_name='Created at', default=timezone.now)
+    updated_at = models.DateTimeField(verbose_name='Updated at', blank=True, null=True)
