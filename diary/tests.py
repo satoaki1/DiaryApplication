e694c9d6440913dd6c46f033aeca76b11d2576d0
ihsa332019 <satoakiishihara@sd.taylors.edu.my>
2022-12-20 18:23:58 +0900

    newly created accounts app.

--- diary/tests.py
+++ diary/tests.py
@@ -1,3 +1,22 @@
+import datetime
 from django.test import TestCase
+from .models import Diary
 
 # Create your tests here.
+class DiaryModelTests(TestCase):
+
+    def test_diary_has_date(self):
+        """
+        Confirm whether created diary data has datetime
+        """
+        Diary.objects.create(title='test_title', text='test_text')
+        actual_diary = Diary.objects.get(title='test_title')
+        self.assertIsInstance(actual_diary.date, datetime.date)
+
+    def test_save_and_retrieve(self):
+        """
+        confirm the save and retrieve of diary data
+        """
+        Diary.objects.create(title='test_title', text='test_text')
+        actual_diary = Diary.objects.get(title='test_title')
+        self.assertEqual(actual_diary.title, 'test_title')
